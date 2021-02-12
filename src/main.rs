1ff3bba2f4172ebb1958a7ee815497e62370cfc7
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2021-02-12 17:59:02 +0100

    Add datastructures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+enum Condition {
+    True,
+    False,
+    Or(Box<Condition>, Box<Condition>),
+    And(Box<Condition>, Box<Condition>),
+    Less(&'static str, &'static str),
+    Equal(&'static str, &'static str),
+    More(&'static str, &'static str)
+}
+
+enum Expression {
+    Table,
+    Selection(Box<Expression>, Box<Condition>),
+    Project(Vec<&'static str>, Box<Expression>),
+    Renaming(Vec<&'static str>, Vec<&'static str>, Box<Expression>),
+    Minus(Box<Expression>, Box<Expression>),
+    Union(Box<Expression>, Box<Expression>),
+    Load(&'static str)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, worldd!");
 }
