82d11af6ce64f79f75ab507c8d1a989379acbad8
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2021-02-26 17:15:31 +0100

    parse ints as ints

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use crate::eval::*;
 use crate::output::*;
 
 fn main() {
-    let v = Box::new(Expression::Load(String::from("project_spec/samples/employes.csv")));
+    let v = Box::new(Expression::Project(Box::new(Expression::Load(String::from("project_spec/samples/employes.csv"))), vec![String::from("dpt"), String::from("email")]));
     let _x = eval(v);
     print_table(_x);
 }
