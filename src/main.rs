941a847f4623371e2a5656042a7f3cc9d3489f48
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2021-02-26 20:23:38 +0100

    well no

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ use crate::eval::*;
 use crate::output::*;
 
 fn main() {
-    let v = Box::new(Expression::Project(Box::new(Expression::Load(String::from("project_spec/samples/employes.csv"))), vec![String::from("dpt"), String::from("email")]));
+    let v = Box::new(Expression::Product(
+        Box::new(Expression::Project(
+            Box::new(Expression::Load(String::from("project_spec/samples/employes.csv"))), 
+            vec![String::from("dpt"), String::from("email")]
+        )),
+        Box::new(Expression::Load(String::from("project_spec/samples/membres.csv")))
+    ));
+
+    
     let _x = eval(v);
     print_table(_x);
 }
