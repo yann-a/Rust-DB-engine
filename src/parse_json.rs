e14c84b16bab77d4e0dc4e32c5dfe034cdb14566
Yann Aguettaz <yann.aguettaz@ens-lyon.fr>
2021-02-26 20:24:52 +0100

    fill parse

--- src/parse_json.rs
+++ src/parse_json.rs
@@ -1,9 +1,10 @@
 use crate::types::*;
 
 pub fn read_json() -> Expression {
-    let file = File::open(path)?;
+    
+    /* let file = File::open(path)?;
     let reader = BufReader::new(file);
 
     // Read the JSON contents of the file as an instance of `User`.
-    let u = serde_json::from_reader(reader)?;
+    let u = serde_json::from_reader(reader)?; */
 }
